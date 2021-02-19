94e4f102292ff904e8bd509f91eb91322b57c914
Junaid Ahmed <junaidkbr@gmail.com>
2021-02-20 01:44:40 +0500

    Add test to check ID uniqueness

--- test/data.test.js
+++ test/data.test.js
@@ -1,12 +1,17 @@
 /* eslint-disable */
 const data = require('../src/scripts/data/data.json');
+let tasbihIds = [];
 
 test('verifying tasbihaat data to be an array', () => {
   expect(Array.isArray(data)).toBe(true);
 });
 
 data.forEach((tasbih) => {
-  test(`verifying tabih with id ${tasbih.id} to be a valid object`, () => {
+  test(`verifying tasbih with id ${tasbih.id} to be unique`, () => {
+    expect(tasbihIds.includes(tasbih.id)).toBe(true);
+  });
+
+  test(`verifying tasbih with id ${tasbih.id} to be a valid object`, () => {
     expect(typeof tasbih.id).toBe('number');
     expect(typeof tasbih.content).toBe('string');
     expect(Array.isArray(tasbih.category)).toBe(true);
@@ -15,4 +20,6 @@ data.forEach((tasbih) => {
     expect(typeof tasbih.translations.en).toBe('string');
     expect(typeof tasbih.translations.ur).toBe('string');
   });
+
+  tasbihIds.push(tasbih.id);
 });
